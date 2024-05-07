2f5226a411bc7c8b54f4a526ee747d400fd804a8
Victor <victorgabmariano@gmail.com>
2024-05-07 19:04:30 -0300

    fix: add missing dependencies and correct lazygit installation

--- install.sh
+++ install.sh
@@ -12,6 +12,18 @@ install_package() {
 	fi
 }
 
+# Function to install npm package and handle errors
+npm_install() {
+	package_name=$1
+	echo "Installing $package_name..."
+	if npm install -g $package_name; then
+		echo "Successfully installed $package_name"
+	else
+		echo "Error installing $package_name"
+		errors+=("$package_name")
+	fi
+}
+
 # Array to store errors
 errors=()
 
@@ -22,16 +34,20 @@ sudo apt update
 sudo apt upgrade -y
 
 # Install packages
+install_package "fd-find"
 install_package "xclip"
 install_package "python3-pip"
 install_package "ripgrep"
 
-# Add lazygit repository
-sudo add-apt-repository ppa:lazygit-team/release
-sudo apt-get update
+# Add lazygit repository and install it
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin/
 
-# Install lazygit
-install_package "lazygit"
+# install npm packages
+npm_install "neovim"
+npm_install "tree-sitter-cli"
 
 # Display errors
 if [ ${#errors[@]} -ne 0 ]; then
